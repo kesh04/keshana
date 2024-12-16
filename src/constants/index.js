2dc09ac854a1e1805bf2ddd3cec6e43ef5eb905a
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2024-12-16 08:23:41 +0530

    new changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,13 +4,24 @@ import project3 from "../assets/projects/bus.jpg";
 import project4 from "../assets/projects/kes.jpg";
 import project5 from "../assets/projects/cer.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React, React Native and Next.js, as well as back-end technologies like Node.js, MySQL, Express.js, Firebase and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React, React Native and Next.js, as well as back-end technologies like Node.js, MySQL, Express.js, Firebase and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, React Native, Firebase and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2024/03/15 - Present",
+
+    
+    year: "2024/08/01 - Present",
+    role: "Software Engineer ",
+    company: "The Colors",
+    description: `Developing and Maintaining web applications using JavaScript, React.js, PHP, Laravel and Node.js. Implemented RESTful APIs and integrated with MySQL database. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: [ "React.js", "Node.js", "Express.js", "MySQL", "PHP" ,"Laravel"],
+  },
+  {
+
+
+    year: "2024/03/15 - 2024/07/28",
     role: "Software Engineer ",
     company: "CEEET",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
